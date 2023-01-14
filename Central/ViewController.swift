f837ae9b31a1938c26d67e2cb4aaf36bc6c28d0a
daniel-rodriguez-yonusa <daniel.rodriguez@yonusa.com>
2023-01-14 15:15:36 -0600

    Initial Commit

---
+++ Central/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Central
+//
+//  Created by Daniel Alberto Rodriguez Cielo on 14/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
